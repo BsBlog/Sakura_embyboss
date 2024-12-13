38534723692b7b9985966a116130d1c98a4b232d
Bnq Dzj <64721413+BnqDzj@users.noreply.github.com>
2024-12-13 13:34:46 +0800

    import modules

--- bot/modules/commands/__init__.py
+++ bot/modules/commands/__init__.py
@@ -9,4 +9,4 @@ from .score_coins import score_user, coins_user
 from .start import ui_g_command, my_info, count_info, p_start, b_start, store_alls
 from .syncs import sync_emby_group, sync_emby_unbound, bindall_id, reload_admins
 from .view_user import list_whitelist, whitelist_page, list_normaluser, normaluser_page
-from .rob import get_lock, delete_msg_with_error, change_emby_amount, countdown, start_rob, show_onlooker_message, update_edit_message, get_buttons, onlookers, surrender, fighting, handle_watch_rewards, handle_rob_callback, rob_user, get_fullname_with_link
+from . import rob
